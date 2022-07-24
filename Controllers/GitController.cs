b2bd3042ddde513f3710c542fee0536b21983600
yashjain <yashjain09488@gmail.com>
2022-07-24 15:35:51 +0530

    New Controller

---
+++ Controllers/GitController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC_APP_EF_DB_First.Controllers
+{
+    public class GitController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
